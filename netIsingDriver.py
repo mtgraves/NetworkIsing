19e7edb8caef94cebaef07b1d66d69621650efba
Max Graves <mtg6193@gmail.com>
2013-04-02 16:09:58 -0400

    got energy calculations working (I think, not yet tested) and also spins are able to be passed from up list to down list

--- netIsingDriver.py
+++ netIsingDriver.py
@@ -21,27 +21,57 @@ def parseCMD():
             help="show the simulation in histogram form (SLOW!)")
     parser.add_argument('--nodes', '-N', type=int, default=10,
             help='number of nodes in network')
-    parser.add_argument('--temp', '-T', type=float,
+    parser.add_argument('--temp', '-T', type=float, default = 5.0,
             help='enter temperature in kelvin')
     parser.add_argument('--field', '-H', type=float, default=0.0,
             help='enter magnetic field strength')
-    parser.add_argument('--length', '-L', type=float,
-            help='enter the length of the 2-d lattice')
     parser.add_argument('--exchange', '-J', type=float, default=1,
             help='enter the value of the exchange constant')
-    parser.add_argument('--sweeps', '-s', type=int,
+    parser.add_argument('--sweeps', '-s', type=int, default = 2,
             help='enter the number of MC sweeps')
     return parser.parse_args()
+
+def swapSpinInList(spinUp, spinDown, spin):
+    """
+    Removes a node index from one list and puts it in the other.
+    """
+    if spin in spinDown:
+        spinDown.remove(spin)
+        spinUp.append(spin)
+    else:
+        spinUp.remove(spin)
+        spinDown.append(spin)
+    
+    return spinUp, spinDown
+
+def EnergyChange(spinUp, G, J, R):
+    """
+    calculates the energy change of flipping a spin.
+    """
+    if R in spinUp:
+        tempSpin = 1.0
+    else:
+        tempSpin = -1.0
+    spinSum = 0.0
+    for i in G[R]:
+        spinSum += tempSpin*G[R][i]
+
+    delE =  2.0 * J * spinSum
+    return delE
 
 # =============================================================================
 def main():
 
+    # assign variables, define constants
     args = parseCMD()
+    T,H,N = float(args.temp), float(args.field), int(args.nodes)
+    J, s = float(args.exchange), int(args.sweeps)
+    k_B = 1     # = 1.3806503 * pow(10,-23)
 
     # Define graph.  For complete list of predefined graphs, see:
     # http://networkx.github.com/documentation/latest/tutorial/
     #   tutorial.html#adding-attributes-to-graphs-nodes-and-edges
-    G = nx.complete_graph(args.nodes)
+    G = nx.complete_graph(N)
 
     # keep track of spins of nodes
     spinUp, spinDown = [], []
@@ -58,15 +88,30 @@ def main():
                 G[neighbor][node] = r
             else:
                 pass
+    
+    # define arrays for mcSteps, Energies, Magnetism
+    mcSteps, Es, Ms = pl.arange(s), pl.array([]), pl.array([])
+    #E2 = np.array([E*E]) 
 
+    for step in mcSteps:
+        # randomly choose a node to try to flip the spin
+        R = random.randint(0,len(G)-1)
+        
+        # compute change in energy from flipping that spin
+        delE = EnergyChange(spinUp, G, J, R)
+        
+        sys.exit()
+    spin = 2
+    spinUp, spinDown = swapSpinInList(spinUp, spinDown, spin)
+    
     # plot the network with colored nodes indicating spin 
     fig1 = pl.figure(1)
     p1 = fig1.add_subplot(111)
     position = nx.circular_layout(G)
-    nx.draw_networkx_nodes(G,position, nodelist=spinUp, node_color='green')
-    nx.draw_networkx_nodes(G,position, nodelist=spinDown, node_color='purple')
+    nx.draw_networkx_nodes(G,position, nodelist=spinUp, node_color='black')
+    nx.draw_networkx_nodes(G,position, nodelist=spinDown, node_color='pink')
     nx.draw_networkx_edges(G,position)
-    nx.draw_networkx_labels(G,position)
+    #nx.draw_networkx_labels(G,position)
      
     pl.show()
 
